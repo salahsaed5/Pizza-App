6bd9efeb88b657c21a856db09e7acf7dfa8bfa0f
abdo1717 <abdoaeed663@yahoo.com>
2023-04-18 01:21:53 +0200

    validation and some edit

--- Food_proj/Navigations/Index.js
+++ Food_proj/Navigations/Index.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View , SafeAreaView} from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -9,41 +9,37 @@ import GetStart2 from '../components/GetStart2';
 import GetStart3 from '../components/GetStart3';
 import signup from '../components/signup'
 import ForgetPassword from '../components/ForgetPassword';
-import Profaile from '../components/Profaile'
-import UpdateProfile from '../components/UpdateProfile'
-import Home from '../components/Home'
+import Profaile from '../components/profaile';
+
 const Stack = createNativeStackNavigator();
 
 
 const Index = () => {
   return (
     <NavigationContainer>
-        <SafeAreaView style={styles.root}>
+      <SafeAreaView style={styles.root}>
 
-     <Stack.Navigator >
-      <Stack.Screen name="GetStart" component={GetStart} />
-      <Stack.Screen name="GetStart2" component={GetStart2} />
-      <Stack.Screen name="GetStart3" component={GetStart3} />
-      <Stack.Screen name="Login" component={Login} /> 
-      <Stack.Screen name="ForgetPassword" component={ ForgetPassword } />
-      <Stack.Screen name="signup" component={signup} />
-      <Stack.Screen name="Profaile" component={Profaile} /> 
-      <Stack.Screen name="UpdateProfile" component={UpdateProfile} />
-      <Stack.Screen name="Home" component={Home} />
+        <Stack.Navigator >
+          <Stack.Screen name="GetStart" component={GetStart} />
+          <Stack.Screen name="GetStart2" component={GetStart2} />
+          <Stack.Screen name="GetStart3" component={GetStart3} />
+          <Stack.Screen name="Login" component={Login} />
+          <Stack.Screen name="ForgetPassword" component={ForgetPassword} />
+          <Stack.Screen name="signup" component={signup} />
+          <Stack.Screen name="Profaile" component={Profaile} />
 
 
-
-    </Stack.Navigator>
-    </SafeAreaView>
-  </NavigationContainer>
+        </Stack.Navigator>
+      </SafeAreaView>
+    </NavigationContainer>
   )
 }
 const styles = StyleSheet.create({
-    root: {
-        flex: 1,
-    },
-  
-  
-  });
+  root: {
+    flex: 1,
+  },
+
+
+});
 
 export default Index
--- Food_proj/components/signup.js
+++ Food_proj/components/signup.js
@@ -1,10 +1,10 @@
-import React, { useState,useEffect } from "react";
-import { StyleSheet, Text, View, TextInput, ImageBackground, TouchableOpacity, useWindowDimensions,Image } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, TextInput, ImageBackground, TouchableOpacity, useWindowDimensions, Image } from 'react-native';
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import {auth , provider,db,storag,firebase } from "../firebase/firebase";
+import { auth, provider, db, storag, firebase } from "../firebase/firebase";
 import * as ImagePicker from 'expo-image-picker';
-import { collection, addDoc,and, onSnapshot} from "firebase/firestore";  
-import { doc, setDoc,getDoc,updateDoc,deleteDoc,getDocs} from "firebase/firestore"; 
+import { collection, addDoc, and, onSnapshot } from "firebase/firestore";
+import { doc, setDoc, getDoc, updateDoc, deleteDoc, getDocs } from "firebase/firestore";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 export default function Login({ navigation }) {
@@ -15,7 +15,46 @@ export default function Login({ navigation }) {
     const [phone, setphone] = useState('');
     const [birthdate, setbrithdate] = useState('');
     const [photo, setphoto] = useState(null);
-    const[users,setusers] = useState({});
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [firstnameError, setNameError] = useState('');
+    const [lastnameError, setlastNameError] = useState('');
+
+    const [users, setusers] = useState({});
+
+    const hundelsumbit = () => {
+        let isValid = true;
+
+        if (firstname.trim() === "") {
+            setNameError("Please enter your FirstName");
+            isValid = false;
+        }
+
+        if (lastname.trim() === "") {
+            setlastNameError("Please enter your FirstName");
+            isValid = false;
+        }
+
+        if (email.trim() === "") {
+            setEmailError("Please enter your Email");
+            isValid = false;
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            setEmailError("Please enter a valid Email address");
+            isValid = false;
+        }
+
+        if (password.trim() === "") {
+            setPasswordError("Please enter your Password");
+            isValid = false;
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            setPasswordError("Password must be at least 6 Chracter");
+            isValid = false;
+        }
+
+        if (isValid) {
+            hundleregeister();
+        }
+    };
 
 
     const pickImage = async () => {
@@ -30,37 +69,37 @@ export default function Login({ navigation }) {
         if (!result.canceled) {
             return result.uri;
         }
-      };
-      const updatePhoto = async () => {
+    };
+    const updatePhoto = async () => {
         const uri = await pickImage();
-      console.log(uri);
-      const filename = firstname;
-      const ref = firebase.storage().ref().child("images/" + filename);
-      
-      const response = await fetch(uri);
-      const blob = await response.blob();
-      
-      const snapshot = await ref.put(blob);
-      console.log('Image uploaded successfully');
-      
-      const downloadURL = await snapshot.ref.getDownloadURL();
-      setphoto(downloadURL);
-      
-      }
-      //add data to firestore
-      const add =async()=>{
+        console.log(uri);
+        const filename = firstname;
+        const ref = firebase.storage().ref().child("images/" + filename);
+
+        const response = await fetch(uri);
+        const blob = await response.blob();
+
+        const snapshot = await ref.put(blob);
+        console.log('Image uploaded successfully');
+
+        const downloadURL = await snapshot.ref.getDownloadURL();
+        setphoto(downloadURL);
+
+    }
+    //add data to firestore
+    const add = async () => {
         await setDoc(doc(db, "users", auth.currentUser.uid), {
-          email: email,
-          password: password,
-          lastname:lastname ,
-          firstname:firstname,
-          phone:phone,
-          birthdate:birthdate,
-          photo:photo
-         
+            email: email,
+            password: password,
+            lastname: lastname,
+            firstname: firstname,
+            phone: phone,
+            birthdate: birthdate,
+            photo: photo
+
         });
-        }
-      
+    }
+
 
     const hundleregeister = () => {
         createUserWithEmailAndPassword(auth, email, password)
@@ -87,15 +126,19 @@ export default function Login({ navigation }) {
         <View style={styles.container}>
             <ImageBackground source={image} style={styles.Background}>
                 <View style={styles.loginView}>
+                    <View style={styles.photoView}>
+                        {photo && <Image source={{ uri: photo }} style={styles.photo} />}
+                    </View>
                     <View style={styles.inputView}>
                         <TextInput
                             style={styles.TextInput}
                             placeholder="Email"
                             value={email}
                             placeholderTextColor="#ababab55"
-                            onChangeText={(email) => setEmail(email)}
+                            onChangeText={(email) => { setEmail(email); setEmailError(""); }}
                         />
                     </View>
+                    {emailError ? <Text style={styles.error}>{emailError}</Text> : null}
                     <View style={styles.inputView2}>
                         <TextInput
                             style={styles.TextInput}
@@ -103,27 +146,30 @@ export default function Login({ navigation }) {
                             value={password}
                             placeholderTextColor="#ababab55"
                             secureTextEntry={true}
-                            onChangeText={(password) => setPassword(password)}
+                            onChangeText={(password) => { setPassword(password); setPasswordError(""); }}
                         />
                     </View>
+                    {passwordError ? <Text style={styles.error}>{passwordError}</Text> : null}
                     <View style={styles.inputView2}>
                         <TextInput
                             style={styles.TextInput}
                             placeholder="frist name"
                             value={firstname}
                             placeholderTextColor="#ababab55"
-                            onChangeText={(firstname) => setName(firstname)}
+                            onChangeText={(firstname) => { setName(firstname); setNameError(""); }}
                         />
                     </View>
+                    {firstnameError ? <Text style={styles.error}>{firstnameError}</Text> : null}
                     <View style={styles.inputView2}>
                         <TextInput
                             style={styles.TextInput}
                             placeholder="last name"
                             value={lastname}
                             placeholderTextColor="#ababab55"
-                            onChangeText={(lastname) => setlastName(lastname)}
+                            onChangeText={(lastname) => { setlastName(lastname); setlastNameError("") }}
                         />
                     </View>
+                    {lastnameError ? <Text style={styles.error}>{lastnameError}</Text> : null}
                     <View style={styles.inputView2}>
                         <TextInput
                             style={styles.TextInput}
@@ -134,26 +180,27 @@ export default function Login({ navigation }) {
                         />
                     </View>
                     <View style={styles.inputView2}>
-                    <TextInput
+                        <TextInput
                             style={styles.TextInput}
                             placeholder="phone"
                             value={phone}
+                            keyboardType="numeric"
                             placeholderTextColor="#ababab55"
                             onChangeText={(phone) => setphone(phone)}
-                        
+
                         />
-                        </View>
-                    {photo && <Image source={{ uri: photo }} style={{ width: '15%', height: '15%' }} />}
-                    
+                    </View>
+
+
 
                     <TouchableOpacity style={styles.photoBtn} onPress={updatePhoto}>
                         <Text style={styles.loginText}>set photo</Text>
                     </TouchableOpacity>
-                    
 
-                    
 
-                    <TouchableOpacity style={styles.loginBtn} onPress={hundleregeister}   >
+
+
+                    <TouchableOpacity style={styles.loginBtn} onPress={hundelsumbit}   >
                         <Text style={styles.loginText}>submit</Text>
                     </TouchableOpacity>
 
@@ -185,7 +232,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         width: '80%',
         height: '7%',
-        marginTop: '55%',
+        marginTop: '5%',
         marginBottom: "4%",
 
     },
@@ -204,7 +251,7 @@ const styles = StyleSheet.create({
     },
     loginBtn: {
         width: "80%",
-        marginTop: '5%',
+        marginTop: '2%',
         marginBottom: '8%',
         borderRadius: 10,
         height: '5%',
@@ -212,12 +259,23 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         backgroundColor: "#ED7014",
     },
+    photoView: {
+        marginTop: '20%',
+        backgroundColor: '#85858629',
+        borderRadius: 50,
+        width: '30%',
+        height: '15%',
+    },
+    photo: {
+        borderRadius: 50,
+        width: '100%',
+        height: '100%',
+    },
     photoBtn: {
-        width: "15%",
-        marginTop: '5%',
+        width: "80%",
         borderRadius: 10,
         height: '5%',
-         alignItems: "center",
+        alignItems: "center",
         justifyContent: "center",
         backgroundColor: "#ED7014",
     },
@@ -227,5 +285,6 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
 
     },
+    error: {},
 });
 
