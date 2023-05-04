2fc377432f250a39376b5bcca1f3170655a2a93c
Hassan24566 <126582565+Hassan24566@users.noreply.github.com>
2023-05-05 00:10:02 +0200

    modify 1

--- Food_proj/Screens/Admin.js
+++ Food_proj/Screens/Admin.js
@@ -59,6 +59,7 @@ export default function UpdateProfile({ navigation }) {
             photo: photo
 
         });
+        navigation.navigate('Home');
     }
 
     const updatAlldata = async () => {
